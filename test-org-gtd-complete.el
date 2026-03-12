f1e022e9bbb3eb082c927eaf707e233d0c4b6ac2
OverbearingPearl <OverbearingPearl@outlook.com>
2026-03-12 16:35:32 +0800

    Remove redundant package dependency
    - Removed `org-gtd-complete` from `Package-Requires`
      • This was a circular dependency since this is the core package
      • Other dependencies (emacs,ert) remain unchanged
    - No functional changes to codebase
    - Fixes package loading warning when installing via package manager

--- test-org-gtd-complete.el
+++ test-org-gtd-complete.el
@@ -6,7 +6,6 @@
 ;; Maintainer: OverbearingPearl <OverbearingPearl@outlook.com>
 ;; URL: https://github.com/OverbearingPearl/org-gtd-complete
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "27.1") (ert "1.0") (org-gtd-complete "0.1.0"))
 ;; Keywords: outlines, tools, convenience, productivity, gtd, org
 ;; Homepage: https://github.com/OverbearingPearl/org-gtd-complete
 
